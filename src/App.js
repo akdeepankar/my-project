30cae07a7d7c045931fc8351da92747b6c50951a
AK DEEPANKAR <akdeepaknyc@gmail.com>
2023-05-25 19:26:56 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import { Avatar, Card, Grid, Button, Divider, Tooltip } from "@nextui-org/react";
+import { Avatar, Card, Grid, Button, Image, Tooltip } from "@nextui-org/react";
+import { a } from "./content";
+import { data } from "./content";
+import { useState } from 'react';
+
 
 
 
 function App() {
+
+  const [index, setIndex] = useState(0);
+
+  let hasPrev = index > 0;
+  let hasNext = index < data.length - 1;
+
+  function handlePrevClick() {
+    if (hasPrev) {
+      setIndex(index - 1);
+    }
+  }
+
+  function handleNextClick() {
+    if (hasNext) {
+      setIndex(index + 1);
+    }
+  }
+
+  let datalist = data[index];
+
+
   return (
     <div className="App">
       <main className=' md:pl-40 md:pr-40 bg-white px-5 pt-5 pb-5' style={{ backgroundImage:`url(${'https://nextui.org/gradient-left-dark.svg'})`, backgroundPosition: '-100px'  }}>
-        <section className="pt-10">
+        <section className="pt-10 bg-yellow-300">
           <h1 className='md:text-6xl text-4xl font-bold'>MySpace.</h1>
         </section>
 
@@ -66,7 +91,7 @@ function App() {
           <div>
              
                  <div className='h-60 w-80 pt-5'>
-                  <img src="https://img.freepik.com/free-vector/manager-prioritizing-tasks-list_74855-5272.jpg?w=1060&t=st=1685016458~exp=1685017058~hmac=586e3af8ac670340935f158e3530963700b5fdb65e3ca35321dfa749b799b64a" alt=""></img>
+                  <img src="pic1.png" alt=""></img>
             </div>
         
           </div>
@@ -76,7 +101,7 @@ function App() {
 
         <section className='md:flex pt-10 justify-evenly'>
           <div className="p-2">
-           <img alt="" className='rounded-xl h-80' src='https://images.pexels.com/photos/2115217/pexels-photo-2115217.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'></img>
+           <img alt="" className='rounded-xl h-80' src={datalist.pics}></img>
           </div>
           <div className='p-2'>
           <Card
@@ -86,13 +111,15 @@ function App() {
         }}>
       <Card.Body>
       <div className='w-90 h-65'>
-            <h1 className='font-bold text-2xl'>Heading</h1>
-            <h1>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the </h1>
+            <h1 className='font-bold text-2xl'>{datalist.heading}</h1>
+            <h1>{datalist.desc}</h1>
             <div className='flex pt-2 justify-between'>
-            <Button color="primary" auto>
+            <Button onClick={handlePrevClick}
+        disabled={!hasPrev} color="primary" auto>
               Prev
             </Button>
-            <Button color="primary" auto>
+            <Button onClick={handleNextClick}
+        disabled={!hasNext} color="primary" auto>
               Next
             </Button>
             </div>
@@ -103,10 +130,11 @@ function App() {
           
         </section>
 
-        <section className="md:flex justify-evenly">
-            <div className="pt-10">
-              <h1 className='text-4xl font-bold'>Privacy at Its Peak.</h1>
-              <div className="md:pl-10 pt-5 flex justify-between md:justify-around w-60">
+  
+        <section className="pb-5">
+            <div className="pt-10 justify-center">
+              <h1 className='text-4xl text-center font-bold'>Privacy at Its Peak.</h1>
+              <div className="md:pl-10 pt-10 flex justify-evenly pb-5">
                 <Tooltip placement="top" content="Developers love Next.js">
                   <Avatar
                     pointer
@@ -137,23 +165,37 @@ function App() {
                     squared
                   />
                 </Tooltip>
+                
               </div>
-              <h1 className="text-l font-semibold pt-2 md:pl-8">Hey. Hover over us for Secrets.</h1>
+              <h1 className="text-center text-l font-semibold pt-2 ">Hey. Hover over us for Secrets.</h1>
             </div>
 
-            <div className="h-60 w-80">
-            <img src="https://img.freepik.com/free-vector/cloud-computing-security-abstract-concept-illustration_335657-2105.jpg?w=740&t=st=1685017671~exp=1685018271~hmac=d8b5263fcf35e7f063a76458a6e3cf9de39262246981357149c72dd11e89574f" alt=""></img>
-            </div>
         </section>
 
         <section>
-          <div className="pt-5">
-          <h1 className='text-4xl font-bold'>Try Here</h1>
-          <div className="pt-2 flex justify-around">
+        <h1 className='text-4xl text-center font-bold pt-5'>Don't Wait Download Now!</h1>
+
+        <div className='flex justify-between pl-2 pt-2'>
+
+                 <div className='flex p-10'>
+                    {a.map(i => {
+                      return (  <Image
+                      size="sm"
+                      src={i}                      
+                      />);
+                      })}
+                
+                 </div>
            
-          </div>
-          </div>
+            </div>
         </section>
+
+
+        <section className="bg-yellow-300 pt-1">
+          <h1 className="text-center text-s font-semibold">MySpace by AK DEEPANKAR</h1>
+        </section>
+
+      
 
 
       </main>
